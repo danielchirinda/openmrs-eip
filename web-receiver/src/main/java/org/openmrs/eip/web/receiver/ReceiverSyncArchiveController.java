3561dc39b0c14209ed6e7cc090fbbd9eedbd33d6
Daniel Chirinda <chirindzadaniel@gmail.com>
2022-10-27 17:24:38 +0200

    add unit tests

--- web-receiver/src/main/java/org/openmrs/eip/web/receiver/ReceiverSyncArchiveController.java
+++ web-receiver/src/main/java/org/openmrs/eip/web/receiver/ReceiverSyncArchiveController.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -29,5 +30,16 @@ public class ReceiverSyncArchiveController extends BaseRestController {
 		}
 		
 		return doGetAll();
+	}
+	
+	@GetMapping
+	@RequestMapping(params = { "startDate", "endDate" })
+	public Map<String, Object> searchByPeriod(@RequestParam(name = "startDate") String startDate,
+	        @RequestParam(name = "endDate") String endDate) {
+		if (log.isDebugEnabled()) {
+			log.debug("Fetching archived events: ");
+		}
+		
+		return doSearchByPeriod(startDate, endDate);
 	}
 }
---
+++ web-receiver/src/test/java/org/openmrs/eip/web/receiver/ReceiverSyncArchiveControllerTest.java
@@ -0,0 +1,69 @@
+package org.openmrs.eip.web.receiver;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.openmrs.eip.app.SyncConstants.MGT_DATASOURCE_NAME;
+import static org.openmrs.eip.app.SyncConstants.MGT_TX_MGR;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import org.openmrs.eip.app.management.entity.receiver.ReceiverSyncArchive;
+import org.openmrs.eip.app.receiver.BaseReceiverTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+
+@Sql(scripts = { "classpath:mgt_site_info.sql",
+        "classpath:mgt_receiver_sync_archive.sql" }, config = @SqlConfig(dataSource = MGT_DATASOURCE_NAME, transactionManager = MGT_TX_MGR))
+public class ReceiverSyncArchiveControllerTest extends BaseReceiverTest {
+	
+	@Autowired
+	private ReceiverSyncArchiveController controller;
+	
+	@Test
+	public void shouldGetAllArchiveMessages() {
+		Map result = controller.getAll();
+		assertEquals(2, result.size());
+		assertEquals(3, result.get("count"));
+		assertEquals(3, ((List) result.get("items")).size());
+		assertEquals("e2bc25aa-1d5f-11e0-b929-000c29ad1d07",
+		    ((List<ReceiverSyncArchive>) result.get("items")).get(0).getIdentifier());
+		assertEquals("4316548b-8803-43b7-bd10-49f26bc26dde",
+		    ((List<ReceiverSyncArchive>) result.get("items")).get(0).getMessageUuid());
+		
+	}
+	
+	@Test
+	public void shouldDoSearchByPeriod() {
+		String stardDate = "2022-10-25";
+		String endDate = "2022-10-30";
+		
+		Map results = controller.doSearchByPeriod(stardDate, endDate);
+		assertEquals(1, results.get("count"));
+		assertNotNull(((List<ReceiverSyncArchive>) results.get("items")).get(0).getDateCreated());
+		assertNotNull(((List<ReceiverSyncArchive>) results.get("items")).get(0).getDateReceived());
+		assertNotNull(((List<ReceiverSyncArchive>) results.get("items")).get(0).getDateSentBySender());
+		
+		stardDate = "";
+		endDate = "2022-10-23";
+		
+		Map result = controller.doSearchByPeriod(stardDate, endDate);
+		assertEquals(1, results.get("count"));
+		assertEquals("e2bc25aa-1d5f-11e0-b929-000c29ad1d07",
+		    ((List<ReceiverSyncArchive>) result.get("items")).get(0).getIdentifier());
+		assertNotNull(((List<ReceiverSyncArchive>) result.get("items")).get(0).getDateCreated());
+		assertNotNull(((List<ReceiverSyncArchive>) result.get("items")).get(0).getDateReceived());
+		assertNotNull(((List<ReceiverSyncArchive>) result.get("items")).get(0).getDateSentBySender());
+		
+		stardDate = "2022-10-30";
+		endDate = "2022-11-01";
+		
+		Map res = controller.doSearchByPeriod(stardDate, endDate);
+		assertEquals(0, res.get("count"));
+		assertEquals(0, ((List) res.get("items")).size());
+		assertEquals(2, result.size());
+	}
+	
+}
